7304deff246ed6d9853099959d6eff0ef68e5aa7
climaco-sarmiento <118277981+climaco-sarmiento@users.noreply.github.com>
2023-08-07 11:42:30 +0100

    importing libraries

--- exercises/PlaceGroup-v1/PlaceGroup-v1/Class1.cs
+++ exercises/PlaceGroup-v1/PlaceGroup-v1/Class1.cs
@@ -1,4 +1,15 @@
-﻿namespace PlaceGroup_v1;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Autodesk.Revit.DB.Architecture;
+namespace PlaceGroup_v1;
 
 public class Class1
 {
